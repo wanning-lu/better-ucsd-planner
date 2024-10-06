98c84074f16fef445623a41ad2b6b76933818971
chezziechester <45701146+chezziechester@users.noreply.github.com>
2024-10-05 21:25:24 -0700

    added checkboxes, nonfunctional for now

--- src/pages/Dropdown.js
+++ src/pages/Dropdown.js
@@ -8,25 +8,40 @@ function Dropdown(props) {
     }
 
     return (
-        <div className="w-5/6 mx-auto p-2 rounded-md cursor-pointer hover:bg-stone-100" onClick={handleOpen}>
+        <div className="w-5/6 mx-auto">
+        <div className="p-2 rounded-md cursor-pointer hover:bg-stone-100" onClick={handleOpen}>
             <div>
                 {/* placeholder for an icon LOL */}
                 {open ? (
-                   <>{'v '}</>
+                    <>{'v '}</>
                 ) : <>{'> '}</>}
                 {/* title of the dropdown and num classes for the requirement */}
                 {props.electiveName ? (
                     <>{props.electiveName} ({props.numRequired})</>
                 ) : <>Core Requirements ({props.classes.length})</>}
             </div>
-            {/* rendering for core classes */}
-            {open && !props.electiveName ? (
-                <div>core</div>
-            ) : null}
-            {/* rendering for elective classes */}
-            {open && props.electiveName ? (
-                <div>elective</div>
-            ) : null}
+        </div>
+        
+        {/* rendering for core classes */}
+  
+        <div className={open && !props.electiveName ? "" : "hidden"}>
+        {props.classes.map((classInfo) => (
+            <label for={classInfo} className="block">
+            <input type="checkbox" name={classInfo} value={classInfo}/>
+            {" " + classInfo}
+            </label>
+        ))}
+        </div>
+        {/* rendering for elective classes */}
+        <div className={open && props.electiveName ? "" : "hidden"}>
+        {props.classes.map((classInfo) => (
+            <label for={classInfo} className="block">
+            <input type="checkbox" name={classInfo} value={classInfo}/>
+            {" " + classInfo}
+            </label>
+        ))}
+        </div>
+
         </div>
     )
 }
