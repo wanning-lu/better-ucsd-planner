a5c1162cac84be545e08235827ac9b3bc3638666
wanning-lu <wanning.lu@gmail.com>
2025-05-06 11:19:01 -0700

    added animation to opening/closing popup

--- src/pages/Popup.js
+++ src/pages/Popup.js
@@ -1,12 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function Popup() {
     const [isOpened, setOpenPopup] = useState(false);
 
+    const arrowRef = useRef(null);
+    const [height, setHeight] = useState("0px");
+    useEffect(() => {
+        if (arrowRef.current) {
+          if (isOpened) {
+            setHeight(`${arrowRef.current.scrollHeight}px`);
+          } else {
+            setHeight("0px");
+          }
+        }
+      }, [isOpened]);
+
+      // (isOpened ? "max-h-screen " : "max-h-0 ") + 
+
     return (
         <div className="w-screen flex flex-col items-center fixed bottom-0 left-0">
             <div className="w-20 bg-gray-300 text-center rounded-t-md hover:cursor-pointer" onClick={() => setOpenPopup(!isOpened)}>^</div>
-            <div className={ (isOpened ? "hidden " : "") + "transition duration-150"}>hewwo</div>
+            <div ref={arrowRef} style={{ maxHeight: height }} className={"transition-all duration-300 ease-out"}>hewwo<br></br>hewwo<br></br>hewwo</div>
         </div>
     )
 }
--- src/pages/ScheduleCourse.js
+++ src/pages/ScheduleCourse.js
@@ -20,7 +20,7 @@ function ScheduleCourse(props) {
     }, [plannedCourses]);
 
     return (
-        <div className="h-10 flex justify-items-center">
+        <div className="h-10 flex justify-items-end w-full">
             <select 
               value = {selectedCourse}
               onChange= {(e) => {
@@ -35,7 +35,7 @@ function ScheduleCourse(props) {
                   }
                 }
               }
-              className={ (keyName in plannedCourses && plannedCourses[keyName].status === 'red' ? "bg-red-400 " : "") + "block w-5/6 rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+              className={ (keyName in plannedCourses && plannedCourses[keyName].status === 'red' ? "bg-red-400 " : "") + "w-5/6 rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
               }
             >
             {selectedCourses.map((course) => {
@@ -45,12 +45,12 @@ function ScheduleCourse(props) {
             })}
           </select>
           {keyName in plannedCourses && plannedCourses[keyName].status === 'red' ? 
-          <div className="flex-1 flex justify-center justify-items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-full size-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
-            </svg>
-          </div> : <></>
-          }
+              <div className="flex-1 flex justify-center justify-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-full size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+                </svg>
+              </div> : <></>
+            }
         </div>
     )
 }
